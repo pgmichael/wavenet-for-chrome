7a17e1c822d8a6613220bd5049f8276c78679101
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2020-01-19 16:42:14 -0500

    Added download shortcut

--- src/typescript/background.ts
+++ src/typescript/background.ts
@@ -3,6 +3,8 @@ const wavenet = new WaveNet()
 
 chrome.commands.getAll((commands) => {
   const startCommand = commands.find((element) => element.name === "speak")
+  const downloadCommand = commands.find((element) => element.name === "download")
+
   chrome.contextMenus.create({
     id: 'start',
     title: `Start Speaking (${startCommand.shortcut})`,
@@ -20,16 +22,37 @@ chrome.commands.getAll((commands) => {
 
   chrome.contextMenus.create({
     id: 'download',
-    title: 'Download as MP3',
+    title: `Download as MP3 (${downloadCommand.shortcut})`,
     contexts: ['selection'],
     onclick: info => wavenet.download(info.selectionText)
   })
 
   chrome.commands.onCommand.addListener((command) => {
-    if (command === "speak")
-      chrome.tabs.executeScript(
-        { code: "window.getSelection().toString();" },
-        (selection) => wavenet.start(selection[0])
-      )
+    switch (command) {
+      case "speak":
+        chrome.tabs.executeScript(
+          { code: convertSelection },
+          (selection) => wavenet.start(selection[0])
+        )
+        break;
+      case "download":
+        chrome.tabs.executeScript(
+          { code: convertSelection },
+          (selection) => wavenet.download(selection[0])
+        )
+        break;
+    }
   })
 })
+
+// Get the selected text and adds imgs alt tags into text
+// TODO: Could something like this be possible for the context menu callbacks?
+const convertSelection = `
+    var selectionContents = window.getSelection().getRangeAt(0).cloneContents();
+		var imgs = selectionContents.querySelectorAll('img');
+		for(const img of imgs) {
+			var altText = document.createTextNode(img.alt);
+			img.parentNode.replaceChild(altText, img);
+		}
+		selectionContents.textContent;
+`
