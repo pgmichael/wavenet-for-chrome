58e01e21c3fd67eddea73956b0160fcc3ba43fe5
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2023-07-22 16:18:56 -0400

    Fixed context menu creation error on start and added more logs to help debugging

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,27 +7,32 @@ import { fileExtMap } from './helpers/file-helpers.js'
 let queue = []
 let playing = false
 
-// Bootstrap -------------------------------------------------------------------
-initializeSentry();
+let bootstrappedResolver = null
+const bootstrapped = new Promise((resolve) => (bootstrappedResolver = resolve))
 
-(async function Bootstrap() {
+// Bootstrap -------------------------------------------------------------------
+initializeSentry()
+;(async function Bootstrap() {
   await migrateSyncStorage()
   await handlers.fetchVoices()
   await setDefaultSettings()
   await createContextMenus()
+  bootstrappedResolver()
 })()
 
 // Event listeners -------------------------------------------------------------
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('Handling command', command)
+chrome.commands.onCommand.addListener(function (command) {
+  console.log('Handling command...', ...arguments)
+
   if (!handlers[command]) throw new Error(`No handler found for ${command}`)
   handlers[command]()
 
   return true
 })
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('Handling message', request)
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('Handling message...', ...arguments)
+
   const { id, payload } = request
 
   if (!handlers[id]) throw new Error(`No handler found for ${id}`)
@@ -36,33 +41,38 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   return true
 })
 
-chrome.storage.onChanged.addListener(function(changes) {
-  console.log('Handling storage change', changes)
+chrome.storage.onChanged.addListener(function (changes) {
+  console.log('Handling storage change...', ...arguments)
+
   if (!changes.downloadEncoding) return
 
   updateContextMenus()
 })
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  console.log('Handling context menu click', { info, tab })
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  console.log('Handling context menu click...', ...arguments)
+
   const id = info.menuItemId
   const payload = { text: info.selectionText }
 
   if (!handlers[id]) throw new Error(`No handler found for ${id}`)
-  handlers[id](payload).then((result) => chrome.tabs.sendMessage(tab.id, result))
+  handlers[id](payload).then((result) =>
+    chrome.tabs.sendMessage(tab.id, result)
+  )
 
   return true
 })
 
 // Handlers --------------------------------------------------------------------
 const handlers = {
-  readAloud: async function({ text }) {
+  readAloud: async function ({ text }) {
+    console.log('Reading aloud...', ...arguments)
+
     if (playing) this.stopReading()
+    await createOffscreenDocument()
 
     const chunks = text.chunk()
-    console.log('Reading aloud the following text', chunks)
     queue.push(...chunks)
-    if (playing) return
 
     playing = true
     updateContextMenus()
@@ -79,15 +89,15 @@ const handlers = {
         prefetchQueue.push(this.getAudioUri({ text: nextText, encoding }))
       }
 
-      const audioUri = count === 0
-        ? await this.getAudioUri({ text, encoding })
-        : await prefetchQueue.shift()
+      const audioUri =
+        count === 0
+          ? await this.getAudioUri({ text, encoding })
+          : await prefetchQueue.shift()
 
-      await createOffscreenDocument()
       await chrome.runtime.sendMessage({
         id: 'play',
         payload: { audioUri },
-        offscreen: true
+        offscreen: true,
       })
 
       console.log('Play through of audio complete. Enqueuing next chunk.')
@@ -97,9 +107,14 @@ const handlers = {
     playing = false
     updateContextMenus()
   },
-  readAloudShortcut: async function() {
+  readAloudShortcut: async function () {
+    console.log('Handling read aloud shortcut...', ...arguments)
+
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    const result = await chrome.scripting.executeScript({ target: { tabId: tab.id }, func: retrieveSelection })
+    const result = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: retrieveSelection,
+    })
     const text = result[0].result
 
     if (playing) {
@@ -113,28 +128,39 @@ const handlers = {
       throw error
     })
   },
-  stopReading: async function() {
+  stopReading: async function () {
+    console.log('Stopping reading...', ...arguments)
+
     queue = []
-    chrome.runtime.sendMessage({ id: 'stop', payload: {}, offscreen: true }).catch()
+    chrome.runtime.sendMessage({ id: 'stop', payload: {}, offscreen: true })
     playing = false
     updateContextMenus()
   },
-  download: async function({ text }) {
+  download: async function ({ text }) {
+    console.log('Downloading audio...', ...arguments)
+
     const { downloadEncoding: encoding } = await chrome.storage.sync.get()
     const url = await this.getAudioUri({ text, encoding })
-    await chrome.downloads.download({ url, filename: `something.${fileExtMap[encoding]}` })
+    await chrome.downloads.download({
+      url,
+      filename: `something.${fileExtMap[encoding]}`,
+    })
   },
-  downloadShortcut: async function() {
+  downloadShortcut: async function () {
+    console.log('Handling download shortcut...', ...arguments)
+
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
     const result = await chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: retrieveSelection
+      func: retrieveSelection,
     })
     const text = result[0].result
 
     this.download({ text })
   },
-  synthesize: async function({ text, encoding }) {
+  synthesize: async function ({ text, encoding }) {
+    console.log('Synthesizing text...', ...arguments)
+
     const sync = await chrome.storage.sync.get()
     const voice = sync.voices[sync.language]
     const count = text.length
@@ -144,8 +170,8 @@ const handlers = {
         id: 'error',
         payload: {
           title: 'API key is missing or invalid',
-          message: 'Ensure your API key is valid and try again.'
-        }
+          message: 'Ensure your API key is valid and try again.',
+        },
       })
 
       throw new Error('API key is missing or invalid')
@@ -157,21 +183,28 @@ const handlers = {
       text = undefined
     }
 
-    const response = await fetch(`${await getApiUrl()}/text:synthesize?key=${sync.apiKey}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        audioConfig: { audioEncoding: encoding, pitch: sync.pitch, speakingRate: sync.speed },
-        input: { text, ssml },
-        voice: { languageCode: sync.language, name: voice }
-      })
-    })
+    const response = await fetch(
+      `${await getApiUrl()}/text:synthesize?key=${sync.apiKey}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          audioConfig: {
+            audioEncoding: encoding,
+            pitch: sync.pitch,
+            speakingRate: sync.speed,
+          },
+          input: { text, ssml },
+          voice: { languageCode: sync.language, name: voice },
+        }),
+      }
+    )
 
     if (!response.ok) {
       const message = (await response.json()).error?.message
 
       await sendMessageToCurrentTab({
         id: 'error',
-        payload: { title: 'Failed to synthesize text', message }
+        payload: { title: 'Failed to synthesize text', message },
       })
 
       throw new Error(message)
@@ -189,22 +222,29 @@ const handlers = {
         method: 'post',
         body: {
           count,
-          audioConfig: { audioEncoding: encoding, pitch: sync.pitch, speakingRate: sync.speed },
-          voice: { languageCode: sync.language, name: voice }
-        }
-      })
+          audioConfig: {
+            audioEncoding: encoding,
+            pitch: sync.pitch,
+            speakingRate: sync.speed,
+          },
+          voice: { languageCode: sync.language, name: voice },
+        },
+      }),
     })
 
     return audioContent
   },
-  getAudioUri: async function({ text, encoding }) {
+  getAudioUri: async function ({ text, encoding }) {
+    console.log('Getting audio URI...', ...arguments)
+
     const chunks = text.chunk()
     const promises = chunks.map((text) => this.synthesize({ text, encoding }))
     const audioContents = await Promise.all(promises)
     return `data:audio/${fileExtMap[encoding]};base64,` + audioContents.join()
   },
-  fetchVoices: async function() {
-    console.log('Fetching voices...')
+  fetchVoices: async function () {
+    console.log('Fetching voices...', ...arguments)
+
     try {
       const sync = await chrome.storage.sync.get()
       const baseUrl = await getApiUrl()
@@ -222,20 +262,21 @@ const handlers = {
       return false
     }
   },
-  ping: function() {
-    return 'pong'
-  }
 }
 
 // Helpers ---------------------------------------------------------------------
 async function updateContextMenus() {
   console.log('Updating context menus...', { playing })
 
+  // Prevents context menus from being updated before they are created
+  await bootstrapped
+
   try {
     chrome.contextMenus.update('readAloud', { enabled: true })
     chrome.contextMenus.update('stopReading', { enabled: playing })
 
-    const fileExt = fileExtMap[(await chrome.storage.sync.get()).downloadEncoding]
+    const fileExt =
+      fileExtMap[(await chrome.storage.sync.get()).downloadEncoding]
     const title = `Download ${fileExt.toUpperCase()}`
     chrome.contextMenus.update('download', { title })
   } catch (e) {
@@ -244,12 +285,17 @@ async function updateContextMenus() {
 }
 
 async function createContextMenus() {
-  console.log('Removing and re-creating context menus...')
+  console.log('Creating context menus...', ...arguments)
+
   chrome.contextMenus.removeAll()
 
   const commands = await chrome.commands.getAll()
-  const readAloudShortcut = commands.find(c => c.name === 'readAloudShortcut')?.shortcut
-  const downloadShortcut = commands.find(c => c.name === 'downloadShortcut')?.shortcut
+  const readAloudShortcut = commands.find(
+    (c) => c.name === 'readAloudShortcut'
+  )?.shortcut
+  const downloadShortcut = commands.find(
+    (c) => c.name === 'downloadShortcut'
+  )?.shortcut
 
   let title = 'Read aloud'
   if (readAloudShortcut) title += ` (${readAloudShortcut})`
@@ -257,7 +303,7 @@ async function createContextMenus() {
     id: 'readAloud',
     title,
     contexts: ['selection'],
-    enabled: !playing
+    enabled: !playing,
   })
 
   title = 'Stop reading'
@@ -266,7 +312,7 @@ async function createContextMenus() {
     id: 'stopReading',
     title,
     contexts: ['all'],
-    enabled: playing
+    enabled: playing,
   })
 
   const fileExt = fileExtMap[(await chrome.storage.sync.get()).downloadEncoding]
@@ -275,28 +321,30 @@ async function createContextMenus() {
   chrome.contextMenus.create({
     id: 'download',
     title,
-    contexts: ['selection']
+    contexts: ['selection'],
   })
 }
 
 async function createOffscreenDocument() {
-  console.log('Creating offscreen document...')
-  const path = 'assets/offscreen.html'
-  if (await hasOffscreenDocument(path)) return
+  console.log('Creating offscreen document...', ...arguments)
 
-  try {
-    await chrome.offscreen.createDocument({
-      url: 'assets/offscreen.html',
-      reasons: [chrome.offscreen.Reason.AUDIO_PLAYBACK],
-      justification: 'Play synthesized audio in the background'
-    })
-  } catch (error) {
-    // Ignore error if document already exists
-    console.warn(error)
+  const documentAlreadyExists = await hasOffscreenDocument(
+    'assets/offscreen.html'
+  )
+  if (documentAlreadyExists) {
+    return
   }
+
+  await chrome.offscreen.createDocument({
+    url: 'assets/offscreen.html',
+    reasons: [chrome.offscreen.Reason.AUDIO_PLAYBACK],
+    justification: 'Plays synthesized audio in the background',
+  })
 }
 
 async function hasOffscreenDocument(path) {
+  console.log('Checking if offscreen document exists...', ...arguments)
+
   const offscreenUrl = chrome.runtime.getURL(path)
   // eslint-disable-next-line no-undef
   const matchedClients = await clients.matchAll()
@@ -310,15 +358,14 @@ async function hasOffscreenDocument(path) {
 }
 
 function initializeSentry() {
-  console.log('Initializing Sentry...')
+  console.log('Initializing Sentry...', ...arguments)
 
   // Nasty hack to make sentry work using Manifest V3
   // https://github.com/getsentry/sentry-javascript/issues/5289#issuecomment-1368705821
   // noinspection JSConstantReassignment
   Sentry.WINDOW.document = {
     visibilityState: 'hidden',
-    addEventListener: () => {
-    }
+    addEventListener: () => {},
   }
 
   Sentry.init({
@@ -327,19 +374,22 @@ function initializeSentry() {
     environment: process.env.ENVIROMENT || 'development',
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
-    beforeSend: function(event) {
+    beforeSend: function (event) {
       if (event.user?.ip_address) delete event.user.ip_address
       if (event.request?.url) delete event.request.url
       return event
-    }
+    },
   })
 }
 
 export async function setDefaultSettings() {
-  console.log('Setting default settings...')
-  await chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' })
-  const sync = await chrome.storage.sync.get()
+  console.log('Setting default settings...', ...arguments)
 
+  await chrome.storage.session.setAccessLevel({
+    accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS',
+  })
+
+  const sync = await chrome.storage.sync.get()
   await chrome.storage.sync.set({
     language: sync.language || 'en-US',
     speed: sync.speed || 1,
@@ -347,12 +397,13 @@ export async function setDefaultSettings() {
     voices: sync.voices || { 'en-US': 'en-US-Wavenet-A' },
     readAloudEncoding: sync.readAloudEncoding || 'OGG_OPUS',
     downloadEncoding: sync.downloadEncoding || 'MP3',
-    apiKey: sync.apiKey || ''
+    apiKey: sync.apiKey || '',
   })
 }
 
 async function migrateSyncStorage() {
-  console.log('Migrating sync storage if needed...')
+  console.log('Migrating sync storage...', ...arguments)
+
   const sync = await chrome.storage.sync.get()
 
   // Extension with version 8 had WAV and OGG_OPUS as a download option, but
@@ -394,7 +445,7 @@ async function migrateSyncStorage() {
 }
 
 async function setLanguages() {
-  console.log('Setting languages...')
+  console.log('Setting languages...', ...arguments)
 
   const session = await chrome.storage.session.get()
 
@@ -402,7 +453,9 @@ async function setLanguages() {
     throw new Error('No voices found. Cannot set languages.')
   }
 
-  const languages = new Set(session.voices.map(voice => voice.languageCodes).flat())
+  const languages = new Set(
+    session.voices.map((voice) => voice.languageCodes).flat()
+  )
 
   await chrome.storage.session.set({ languages: Array.from(languages) })
 
@@ -410,10 +463,13 @@ async function setLanguages() {
 }
 
 function retrieveSelection() {
+  console.log('Retrieving selection...', ...arguments)
+
   const activeElement = document.activeElement
   const selectionContents = window.getSelection()?.getRangeAt(0).cloneContents()
   const imgElements = selectionContents.querySelectorAll('img')
-  const activeElementIsInput = activeElement?.tagName === 'INPUT' || activeElement?.tagName === 'TEXTAREA'
+  const activeElementIsInput =
+    activeElement?.tagName === 'INPUT' || activeElement?.tagName === 'TEXTAREA'
 
   if (activeElementIsInput) {
     const start = activeElement.selectionStart
@@ -431,13 +487,19 @@ function retrieveSelection() {
 }
 
 async function getApiUrl() {
+  console.log('Getting API URL...', ...arguments)
+
   return 'https://texttospeech.googleapis.com/v1beta1'
 }
 
 async function sendMessageToCurrentTab(event) {
+  console.log('Sending message to current tab...', ...arguments)
+
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
   const currentTab = tabs[0]
-  if (!currentTab) throw new Error("No active tab found. Cannot send message to current tab.")
+
+  if (!currentTab)
+    throw new Error('No active tab found. Cannot send message to current tab.')
 
   return chrome.tabs.sendMessage(currentTab.id, event)
 }
