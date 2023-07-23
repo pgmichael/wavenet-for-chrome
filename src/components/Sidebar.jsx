936d21376b004bcd0571a65fd83ec9eeaae53e69
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2023-07-23 13:28:39 -0400

    Added a changelog link to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { classNames } from '../helpers/class-names.js'
 import { Box } from './icons/Box'
 import { Settings } from './icons/Settings'
 import { Star } from './icons/Star'
+import { Help } from './icons/Help'
 import { Github } from './icons/Github.jsx'
 
 export function Sidebar() {
@@ -16,7 +17,9 @@ export function Sidebar() {
           style={{ width: '26px' }}
         />
         <div>
-          <div className="text-base font-bold text-neutral-800 bg-">Wavenet</div>
+          <div className="text-base font-bold text-neutral-800 bg-">
+            Wavenet
+          </div>
           <div
             className="text-xs font-bold text-neutral-500"
             style={{ fontSize: '10px', marginTop: '-5px' }}
@@ -32,12 +35,19 @@ export function Sidebar() {
         Sandbox
       </Sidebar.Item>
       <div className="mt-auto">
+        <Sidebar.Item Icon={Help} color="bg-neutral-400" onClick={() => {
+          const changelogUrl = chrome.runtime.getURL('assets/changelog.html')
+
+          chrome.tabs.create({ url: changelogUrl })
+        }}>
+          Changelog
+        </Sidebar.Item>
         <Sidebar.Item
           Icon={Github}
-          color="bg-neutral-400"
+          color="bg-neutral-600"
           to="https://github.com/pgmichael/wavenet-for-chrome"
         >
-          Documentation
+          Contribute
         </Sidebar.Item>
         <Sidebar.Item
           Icon={Star}
@@ -51,7 +61,7 @@ export function Sidebar() {
   )
 }
 
-Sidebar.Item = function Item({ Icon, children, to, color }) {
+Sidebar.Item = function Item({ Icon, children, to, color, onClick }) {
   const navigate = useNavigate()
   const location = useLocation()
   const active = location.pathname === to
@@ -67,15 +77,15 @@ Sidebar.Item = function Item({ Icon, children, to, color }) {
 
   return (
     <button
-      onClick={handleClick}
+      onClick={onClick || handleClick}
       className={classNames({
         'p-1 flex items-center group font-semibold rounded cursor-pointer transition-all w-full': true,
         'text-neutral-700 hover:text-neutral-900': !active,
-        'bg-neutral-200 bg-opacity-70 text-neutral-900': active
+        'bg-neutral-200 bg-opacity-70 text-neutral-900': active,
       })}
     >
       <div className={`p-1 rounded mr-1.5 text-white ${color}`}>
-        <Icon size={14} className="group-hover:animate-wiggle"/>
+        <Icon size={14} className="group-hover:animate-wiggle" />
       </div>
       <div>{children}</div>
     </button>
