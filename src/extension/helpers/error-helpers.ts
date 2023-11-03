e41ddccb71d0c87a22bfbea9d789673b2bca8055
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2023-11-03 11:23:52 -0400

    Improved copy for `createGithubIssueFromError`

--- src/extension/helpers/error-helpers.ts
+++ src/extension/helpers/error-helpers.ts
@@ -16,21 +16,38 @@ export function isError(value: any): value is TError {
     return value && value.error === true;
 }
 
-export function createGithubIssueFromError(error) {
-    const extensionVersion = chrome.runtime.getManifest().version;
+export function createGithubIssueFromError(error: TError) {
+    const extensionVersion = chrome.runtime.getManifest().version
+    const userAgent = navigator.userAgent
 
     const issueBody = `
 ## Environment
 
 - **Extension Version:** ${extensionVersion}
+- **Browser/User Agent:** ${userAgent}
 
 ## Error Details
 
 - **Error Code:** \`${error.errorCode}\`
+- **Error Title:** _"${error.errorTitle}"_
 - **Error Message:** _"${error.errorMessage}"_
-    `;
+
+## Steps to Reproduce
+
+Please provide a detailed list of steps that reproduce the issue.
+
+1. [Step 1]
+2. [Step 2]
+3. [Step 3]
+
+## Additional Information
+
+Provide any additional context about the error, such as what you were doing when it occurred.
+    `
 
     window.open(
-        `https://github.com/pgmichael/wavenet-for-chrome/issues/new?title=${encodeURIComponent(error.errorCode)}&body=${encodeURIComponent(issueBody)}`
-    );
+        `https://github.com/pgmichael/wavenet-for-chrome/issues/new?title=${encodeURIComponent(error.errorTitle)}&body=${encodeURIComponent(issueBody)}`
+    )
 }
+
+
